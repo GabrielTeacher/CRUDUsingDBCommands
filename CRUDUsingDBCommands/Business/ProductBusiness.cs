3273390985dd2930730eb9e4e3e2ff9ef3d6f6ff
GabrielTeacher <98266523+GabrielTeacher@users.noreply.github.com>
2023-03-04 11:40:46 +0200

    Add project files.

---
+++ CRUDUsingDBCommands/Business/ProductBusiness.cs
@@ -0,0 +1,44 @@
+﻿using CRUDUsingDBCommands.Common;
+using CRUDUsingDBCommands.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDUsingDBCommands.Business
+{
+    class ProductBusiness
+    {
+        private ProductData manager = new ProductData();
+
+        public List<Product> GetAll()
+        {
+            return manager.GetAll();
+        }
+        public List<Product> GetTop3()
+        {
+            return manager.GetTop3();
+        }
+
+        public Product Get(int id)
+        {
+            return manager.Get(id);
+        }
+
+        public void Add(Product product)
+        {
+            manager.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            manager.Update(product);
+        }
+
+        public void Delete(int id)
+        {
+            manager.Delete(id);
+        }
+    }
+}
---
+++ CRUDUsingDBCommands/Data/ProductData.cs
@@ -0,0 +1,139 @@
+﻿using CRUDUsingDBCommands.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDUsingDBCommands.Data
+{
+    class ProductData
+    {
+        public List<Product> GetAll()
+        {
+            var productList = new List<Product>();
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("SELECT * FROM product", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                        while (reader.Read())
+                        {
+                            var product = new Product(
+                                reader.GetInt32(0),
+                                reader.GetString(1),
+                                reader.GetDecimal(2),
+                                reader.GetInt32(3)
+                            );
+
+                            productList.Add(product);
+                        }
+
+                }
+                connection.Close();
+            }
+
+            return productList;
+        }
+
+        public Product Get(int id)
+        {
+            Product product = null;
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("SELECT * FROM product WHERE Id=@id", connection);
+                command.Parameters.AddWithValue("id", id);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                        product = new Product(
+                                reader.GetInt32(0),
+                                reader.GetString(1),
+                                reader.GetDecimal(2),
+                                reader.GetInt32(3)
+                        );
+                    }
+                }
+
+                connection.Close();
+            }
+
+            return product;
+        }
+        public List<Product> GetTop3()
+        {
+            var productList = new List<Product>();
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("SELECT TOP 3 * FROM product ORDER BY Stock DESC ", connection);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                            while (reader.Read())
+                    {
+                        var product = new Product(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetDecimal(2),
+                            reader.GetInt32(3)
+                        );
+
+                        productList.Add(product);
+                    }
+
+                }
+                connection.Close();
+            }
+
+            return productList;
+        }
+
+        public void Add(Product product)
+        {
+            using (var connection = Database.GetConnection()){
+                var command = new SqlCommand("INSERT INTO product (Name, Price, Stock) VALUES(@name, @price, @stock)", connection);
+                command.Parameters.AddWithValue("name", product.Name);
+                command.Parameters.AddWithValue("price", product.Price);
+                command.Parameters.AddWithValue("stock", product.Stock);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+
+        public void Update(Product product)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("UPDATE product SET Name=@name, Price=@price, Stock=@stock WHERE Id=@id", connection);
+                command.Parameters.AddWithValue("id", product.Id);
+                command.Parameters.AddWithValue("name", product.Name);
+                command.Parameters.AddWithValue("price", product.Price);
+                command.Parameters.AddWithValue("stock", product.Stock);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = Database.GetConnection())
+            {
+                var command = new SqlCommand("DELETE product WHERE Id=@id", connection);
+                command.Parameters.AddWithValue("id", id);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+    }
+}
